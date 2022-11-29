c7347d83db03c41ba445e1de8e742e505df4cf27
Team1058Communal <mentors@team1058.com>
2022-11-28 20:50:17 -0500

    Cleaned up and fixed logic errors in swerve module

--- src/main/java/frc/robot/SwerveModule.java
+++ src/main/java/frc/robot/SwerveModule.java
@@ -8,25 +8,34 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 public class SwerveModule {
     public WPI_TalonFX speedMotor;
     public WPI_TalonSRX angleMotor;
-    public final int zeroPoint;
+    public final int angleTransform;
     public SwerveModule (int speedMotordeviceid, int angleMotordeviceid, final int zeroAngle, boolean inverted) {
         speedMotor = new WPI_TalonFX(speedMotordeviceid);
         angleMotor = new WPI_TalonSRX(angleMotordeviceid);
-        zeroPoint = zeroAngle;
+        angleTransform = 2048 - zeroAngle;
         angleMotor.configSelectedFeedbackSensor(TalonSRXFeedbackDevice.CTRE_MagEncoder_Absolute, 0, 0);
         angleMotor.configFeedbackNotContinuous(false, 0);
         setPID(1, 0, 0);
         speedMotor.setInverted(inverted);
     }
-    public void setSpeed(double speed) {
-        speedMotor.set(speed);
+    /**
+     * Transform an angle from controller coordinates to motor coordinates
+     * 
+     * @param controller_angle angle given from 0 to 4095
+     * @return transformed angle from 0 to 4095
+     */
+    private int transformAngle(int controller_angle) {
+        return Math.floorMod(controller_angle - angleTransform, 4096);
     }
-    public void setAngle(double angle) {
-        int angleI = (int)(angle * 2048);
-        angleI += zeroPoint;
-        angleMotor.set(ControlMode.Position, angleI);
+    /**
+     * Converts an angle in radians to encoder ticks
+     * 
+     * @param angle_rad angle from -PI to PI
+     * @return angle from 0 to 4095
+     */
+    private static int angleRadToTicks(double angle_rad) {
+        return Math.floorMod(Math.round((angle_rad + Math.PI) / Math.PI  * 2048), 4096); // The floormod is used to turn 4096 into 0
     }
-
     /**
      * Set the angle and speed of the swerve module.
      * 
@@ -34,10 +43,11 @@ public class SwerveModule {
      * @param speed Velocity in m/s
      */
     public void setAngleAndSpeed(double angle, double speed) {
-        int angleI = (int)(angle / Math.PI * 2048 + 2048 + zeroPoint);
-        int current_angle = (int)(angleMotor.getSelectedSensorPosition());
-        int delta_angle = Math.floorMod((angleI - current_angle), 4096)
+        long targetAngle_ticks =  transformAngle(angleRadToTicks(angle)); // range of 0 to 4095
+        int current_angle = (int)(angleMotor.getSelectedSensorPosition()); // current position in raw sensor units
+        int delta_angle = Math.floorMod(targetAngle_ticks - current_angle, 4096); //difference between current angle and target angle
         int target_angle;
+
         if (delta_angle <= 1024) {
             target_angle = current_angle + delta_angle;
         }
@@ -46,15 +56,13 @@ public class SwerveModule {
             target_angle = current_angle + delta_angle - 2048;
         }
         else {
-            target_angle = current_angle + delta_angle + 4096;
+            target_angle = current_angle + delta_angle - 4096;
         }
-
         if (speed != 0) {
             angleMotor.set(ControlMode.Position, target_angle);
         }
-
+    
         speedMotor.set(speed);
-
     }
     public void printAngle()
     {
